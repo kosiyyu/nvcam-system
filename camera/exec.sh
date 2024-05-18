54479af9d7031fe1e0d717cac6312be180a518aa
Karol Kos <kosiyyu@gmail.com>
2024-05-19 00:57:27 +0200

    Make exec.sh run in the background

--- camera/exec.sh
+++ camera/exec.sh
@@ -10,4 +10,12 @@ ip=${ips[0]}
 
 echo -e "Script executed on IP address: ${green_color}$ip${reset_color}"
 
-libcamera-vid --nopreview -t 0 --width 1280 --height 720 --codec h264 --framerate 15 --inline --listen -o "tcp://$ip:8888"
+# libcamera-vid --nopreview -t 0 --width 1280 --height 720 --codec h264 --framerate 15 --inline --listen -o "tcp://$ip:8888"
+
+(libcamera-vid --nopreview -t 0 --width 1280 --height 720 --codec h264 --framerate 15 --inline --listen -o "tcp://$ip:8888" >/dev/null 2>&1 &)
+
+sleep 1
+
+pid=$(pgrep -f "libcamera-vid --nopreview -t 0 --width 1280 --height 720 --codec h264 --framerate 15 --inline --listen -o tcp://$ip:8888")
+
+echo "Camera PID: $pid"
