3d6a415e565c7285dc874f2b0e68f1ab31e9c7cf
Karol Koś <kosiyyu@gmail.com>
2024-05-18 23:09:13 +0200

    Add util scripts

---
+++ utils/scan.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+green_color='\033[0;32m'
+reset_color='\033[0m'
+
+echo
+echo -e "${green_color}This script will scan the local network for active IPs.${reset_color}"
+
+ips=($(hostname -I))
+
+ip=${ips[0]}
+ip=$(ip addr | grep $ip | awk '{print $2}')
+
+result=$(nmap -sn $ip)
+
+found_ips=$(echo "$result" | grep "Nmap scan report" | awk '{print $NF}' | tr -d '()')
+
+echo
+echo "Found IPs:"
+echo -e "${green_color}$found_ips${reset_color}"
---
+++ utils/scan_and_connect.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+green_color='\033[0;32m'
+reset_color='\033[0m'
+
+echo
+echo -e "${green_color}This script will scan the local network for active IPs and attempt to SSH into each one.${reset_color}"
+
+ips=($(hostname -I))
+ip=${ips[0]}
+ip=$(ip addr | grep $ip | awk '{print $2}')
+
+result=$(nmap -sn $ip)
+
+found_ips=$(echo "$result" | grep "Nmap scan report" | awk '{print $NF}' | tr -d '()')
+
+echo -e "Found IPs:"
+echo -e "${green_color}$found_ips${reset_color}"
+
+for i in $found_ips; do
+    echo
+    echo -e "Attempting ssh connection to: ${green_color}$i${reset_color}"
+    ssh -o ConnectTimeout=5 kosiyyu@$i
+done
