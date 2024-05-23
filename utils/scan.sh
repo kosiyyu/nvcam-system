ca7094b6c70cc2a880d205f057bc55ccda5bbbab
Karol Koś <kosiyyu@gmail.com>
2024-05-23 23:44:58 +0200

    Refactor old scripts

--- utils/scan.sh
+++ utils/scan.sh
@@ -5,16 +5,17 @@ reset_color='\033[0m'
 
 echo
 echo -e "${green_color}This script will scan the local network for active IPs.${reset_color}"
+echo
 
+# shellcheck disable=SC2207
 ips=($(hostname -I))
 
 ip=${ips[0]}
-ip=$(ip addr | grep $ip | awk '{print $2}')
+ip=$(ip addr | grep "$ip" | awk '{print $2}')
 
-result=$(nmap -sn $ip)
+result=$(nmap -sn "$ip")
 
 found_ips=$(echo "$result" | grep "Nmap scan report" | awk '{print $NF}' | tr -d '()')
 
-echo
 echo "Found IPs:"
 echo -e "${green_color}$found_ips${reset_color}"
--- utils/scan_and_connect.sh
+++ utils/scan_and_connect.sh
@@ -5,12 +5,14 @@ reset_color='\033[0m'
 
 echo
 echo -e "${green_color}This script will scan the local network for active IPs and attempt to SSH into each one.${reset_color}"
+echo
 
+# shellcheck disable=SC2207
 ips=($(hostname -I))
 ip=${ips[0]}
-ip=$(ip addr | grep $ip | awk '{print $2}')
+ip=$(ip addr | grep "$ip" | awk '{print $2}')
 
-result=$(nmap -sn $ip)
+result=$(nmap -sn "$ip")
 
 found_ips=$(echo "$result" | grep "Nmap scan report" | awk '{print $NF}' | tr -d '()')
 
@@ -20,5 +22,5 @@ echo -e "${green_color}$found_ips${reset_color}"
 for i in $found_ips; do
     echo
     echo -e "Attempting ssh connection to: ${green_color}$i${reset_color}"
-    ssh -o ConnectTimeout=5 kosiyyu@$i
+    ssh -o ConnectTimeout=5 kosiyyu@"$i"
 done
