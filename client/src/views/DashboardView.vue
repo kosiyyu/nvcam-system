2d11ecd7cb7174ed7ec6b74529f9d06e24665eb1
Karol Koś <kosiyyu@gmail.com>
2024-06-02 10:38:35 +0200

    Add image sending

--- client/src/views/DashboardView.vue
+++ client/src/views/DashboardView.vue
@@ -4,6 +4,7 @@ import { ref, onMounted, onUnmounted } from "vue";
 import { io, Socket } from "socket.io-client";
 import { isAuthenticated } from "@/utils/authUtils";
 import { isSocket } from "@/utils/dashboardUtils";
+import axios from "axios";
 
 const ip = import.meta.env.IP;
 
@@ -12,6 +13,46 @@ interface SensorData {
   humidity: string;
   controller_temp: string;
 }
+
+let images = ref<string[]>([]);
+let page = ref(1);
+
+const prevPage = async () => {
+  if (page.value > 1) {
+    const tempPage = page.value - 1;
+    try {
+      await fetchImages(tempPage);
+      page.value = tempPage;
+    } catch (error) {
+      page.value = 1;
+      console.error(error);
+    }
+  }
+};
+
+const nextPage = async () => {
+  const tempPage = page.value + 1;
+  try {
+    await fetchImages(tempPage);
+    page.value = tempPage;
+  } catch (error) {
+    page.value = 1;
+    console.error(error);
+  }
+};
+
+const fetchImages = async (tempPage: number) => {
+  console.log(tempPage);
+  axios(`http://localhost:8003/page/${tempPage}`)
+  .then(response => {
+    const data = response.data;
+    images.value = data.images.map((image: string) => 'data:image/jpeg;base64,' + image);
+  })
+  .catch(error => {
+    console.error(error);
+    page.value = 1;
+  });
+};
 
 let isPlaying = ref(true);
 let imageUrl = ref('');
@@ -84,35 +125,63 @@ function move(direction: string) {
 <template>
   <NavBar :is-authenticated="isAuthenticated" />
   <main class="bg-green-950 flex flex-col items-center h-screen">
+    <div class="flex flex-col items-center">
+        <div class="font-special text-special-pink text-5xl mb-4">
+          Data stream
+        </div>
+      </div>
     <div class="flex space-x-4">
-      <div class="flex flex-col items-center m-4">
+      <div class="flex flex-col items-center m-2">
         <div class="w-[640px] h-[360px] object-cover bg-black">
           <img :src="imageUrl">
+        </div>
+      </div>
+      <div class="mt-2">
+        <div class="w-[300px] bg-special-pink text-black font-bold rounded-lg p-4 mb-2 overflow-auto whitespace-normal">
+          <div>Controller temperature: {{ sensorData.controller_temp }} °C</div>
+          <div>Camera temperature: {{ sensorData.temp }} °C</div>
+          <div>Humidity: {{ sensorData.humidity }} RH</div>
+        </div>
+        <div class="flex">
+          <button @click="move('left')" class="bg-special-pink hover:bg-special-pink text-black font-bold py-1 px-4 rounded-l-full">Move Left</button>
+          <button @click="move('right')" class="bg-special-pink hover:bg-special-pink text-black font-bold py-1 px-4 rounded-r-full">Move Right</button>
         </div>
         <div class="mt-2">
           <div v-if="isPlaying">
             <button @click="pauseVideo"
               class="bg-special-pink hover:bg-special-pink text-black font-bold py-1 px-4 rounded-full">
-              Pause
+              Pause camera stream
             </button>
           </div>
           <div v-else>
             <button @click="playVideo"
               class="bg-special-pink hover:bg-special-pink text-black font-bold py-1 px-4 rounded-full">
-              Play
+              Play camera stream
             </button>
           </div>
         </div>
       </div>
-      <div class="my-4">
-        <div class="w-[300px] bg-special-pink text-black font-bold rounded-lg p-4 mb-2 overflow-auto whitespace-normal">
-          <div>Controller temperature: {{ sensorData.controller_temp }} °C</div>
-          <div>Sensor temperature: {{ sensorData.temp }} °C</div>
-          <div>Sensor humidity: {{ sensorData.humidity }} RH</div>
+    </div>
+      <div class="flex flex-col items-center">
+        <div class="font-special text-special-pink text-5xl mb-4">
+          Search captured frames
         </div>
-        <div class="flex justify-between">
-          <button @click="move('left')" class="bg-special-pink hover:bg-special-pink text-black font-bold py-1 px-4 rounded-full">Move Left</button>
-          <button @click="move('right')" class="bg-special-pink hover:bg-special-pink text-black font-bold py-1 px-4 rounded-full">Move Right</button>
+      </div>
+    <div class="flex flex-col">
+      <div class="flex items-center justify-center">
+        <button @click="prevPage" class="bg-special-pink hover:bg-special-pink text-black font-bold py-1 px-4 rounded-l-full">Previous page</button>
+        <input type="number" v-model.number="page" @change="fetchImages(page)" class="bg-special-pink text-black font-bold py-1 px-4 appearance-none" min="1">
+        <button @click="nextPage" class="bg-special-pink hover:bg-special-pink text-black font-bold py-1 px-4">Next page</button>
+        <button @click="fetchImages(page)" class="bg-special-pink hover:bg-special-pink text-black font-bold py-1 px-4 rounded-r-full">Search</button>
+      </div>
+      <div v-if="images.length !== 0" class="flex-shrink-0 flex overflow-x-scroll space-x-2 mx-2">
+        <div v-for="(image, index) in images" :key="index" class="flex-shrink-0 flex py-2">
+          <img :src="image" class="w-[640px] h-[360px]" />
+        </div>
+      </div>
+      <div v-else class="flex flex-col items-center">
+        <div class="font-special text-special-pink mb-4">
+          No images loaded
         </div>
       </div>
     </div>
@@ -121,3 +190,15 @@ function move(direction: string) {
     footer
   </footer>
 </template>
+
+<style scoped>
+input[type="number"]::-webkit-inner-spin-button,
+input[type="number"]::-webkit-outer-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+input[type="number"] {
+  -moz-appearance: textfield;
+}
+</style>
--- system/main.py
+++ system/main.py
@@ -1,11 +1,15 @@
 import os
+import glob
 import time
 from dotenv import load_dotenv
 import socketio
 import uvicorn
 from starlette.applications import Starlette
 from starlette.middleware.cors import CORSMiddleware
+from starlette.responses import JSONResponse
+from starlette.routing import Route
 from video_stream_server.server import VideoStreamServer
+import base64
 
 ROOM = 'room'
 
@@ -18,7 +22,27 @@ sio = socketio.AsyncServer(
     cors_allowed_origins=["http://localhost:5173"]
 )
 
-star_app = Starlette(debug=True)
+async def get_images(request):
+    page = request.path_params['page']
+    images_per_page = 10
+    start = (page - 1) * images_per_page
+    end = start + images_per_page
+
+    image_files = sorted(glob.glob('./images/*.jpg'))
+    paginated_files = image_files[start:end]
+
+    if not paginated_files:
+        return JSONResponse(status_code=404, content={"detail": "No images found for this page."})
+
+    encoded_images = []
+    for image_file in paginated_files:
+        with open(image_file, 'rb') as file:
+            encoded_string = base64.b64encode(file.read()).decode('utf-8')
+            encoded_images.append(encoded_string)
+
+    return JSONResponse({'images': encoded_images})
+
+star_app = Starlette(debug=True, routes=[Route('/page/{page:int}', get_images, methods=['GET']),])
 star_app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173"],
